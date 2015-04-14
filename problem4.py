8c4721dc24193fc08c4b7760dbd9938bee28ed71
CodingAnarchy <mtanous22@gmail.com>
2015-04-14 00:22:19 -0600

    Problem 4 solution: https://projecteuler.net/problem=4

---
+++ problem4.py
@@ -0,0 +1,13 @@
+def find_max_palindrome(minimum=100, maximum=999):
+    max_palindrome = 0
+    for i in range(maximum, minimum, -1):
+        for r in range(maximum, minimum, -1):
+            x = str(i * r)
+            if x == x[::-1]:
+                if int(x) > max_palindrome:
+                    max_palindrome = int(x)
+
+    return max_palindrome
+
+print find_max_palindrome()
+
