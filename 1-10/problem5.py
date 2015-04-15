99caaaa8a05f0db744f537a234ee5cf67094cc2d
CodingAnarchy <mtanous22@gmail.com>
2015-04-14 20:35:06 -0600

    Solve Problem 5 using the multiplicities of primes approach

---
+++ 1-10/problem5.py
@@ -0,0 +1,14 @@
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+from lib.primes import multiplicity
+
+mults = multiplicity(20)
+result = 1
+for k, v in mults.iteritems():
+    print k, v
+    result *= k ** v
+print result
--- lib/primes.py
+++ lib/primes.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 def primes_sieve(lim):
     a = [True] * lim
     a[0] = a[1] = False
@@ -7,3 +9,19 @@ def primes_sieve(lim):
             yield i
             for n in xrange(i * i, lim, i):
                 a[n] = False
+
+
+def multiplicity(n):
+    # Creating list of primes - ensure lower n to limit memory usage
+    if n > 100000:
+        raise RuntimeError("Use smaller number to prevent memory error")
+    primes = OrderedDict(zip(list(primes_sieve(n)), [1] * n))
+    for n in xrange(4, n + 1):
+        for prime in primes:
+            i = 0
+            while n % prime == 0:
+                i += 1
+                n /= prime
+                if i > primes[prime]:
+                    primes[prime] = i
+    return primes
