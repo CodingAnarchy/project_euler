ccb279c5ee7e9b36f32f89f28452297cba27515c
CodingAnarchy <mtanous22@gmail.com>
2015-08-30 20:40:53 -0600

    Refactor Cython solution for problem 14 to use malloc'd array

--- 10-19/problem14.pyx
+++ 10-19/problem14.pyx
@@ -3,23 +3,26 @@ from libc.stdlib cimport malloc, free
 
 cdef find_max_collatz(unsigned long int max):
     cdef int *collatz_length = <int *>malloc(max * sizeof(int))
-    cdef list TO_ADD # holds numbers of unknown collatz length
+    cdef int *TO_ADD = <int *>malloc(600 * sizeof(int))
     cdef unsigned long iter, j, m, n, s, p, ind, new_length, max_length = 0
 
-    # set initial collatz lengths
+    # set initial collatz lengths and TO_ADD numbers
     iter = 0
     while iter < max:
         collatz_length[iter] = 0
         iter += 1
     collatz_length[1] = 1
+    iter = 0
+    while iter < 600:
+        TO_ADD[iter] = 0
+        iter += 1
 
     # iterate to max and find collatz lengths
     iter = 1
     while iter < max:
         n,s = iter,0
-        TO_ADD = []
         while n > max - 1 or collatz_length[n] < 1:
-            TO_ADD.append(n)
+            TO_ADD[s] = n
             if n % 2 == 0: n = n/2
             else: n = 3*n + 1
             s += 1
@@ -28,6 +31,7 @@ cdef find_max_collatz(unsigned long int max):
         j = 0
         while j < s:
             m = TO_ADD[j]
+            TO_ADD[j] = 0
             if m < max:
                 new_length = collatz_length[n] + s - j
                 collatz_length[m] = new_length
@@ -38,6 +42,7 @@ cdef find_max_collatz(unsigned long int max):
         iter += 1
 
     free(collatz_length)
+    free(TO_ADD)
     return ind
 
 start = time.time()
