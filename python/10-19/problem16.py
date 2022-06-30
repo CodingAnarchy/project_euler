cf92611649f594bee4d05dc848b7a198053e741a
Matt Tanous <matthew.tanous@shopify.com>
2022-06-30 09:11:08 -0400

    Updates for Python 3

--- python/10-19/problem16.py
+++ python/10-19/problem16.py
@@ -2,10 +2,10 @@ import time
 
 
 def pow2sum(exp):
-    pow = list(str(2**1000))
+    pow = list(str(2**exp))
     return sum([int(i) for i in pow])
 
 start = time.time()
 n = pow2sum(1000)
 elapsed = (time.time() - start)
-print "%s found in %s seconds" % (n, elapsed)
+print("%s found in %s seconds" % (n, elapsed))
